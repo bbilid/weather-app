94694202e36e40c2df111bb050e4dc13a1a96ccb
lira31 <lirabiymurzaeva@gmail.com>
2024-02-28 16:11:06 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import NotFoundPage from "./containers/NotFoundPage/NotFoundPage";
+import SearchForm from "./components/SearchForm/SearchForm";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<SearchForm/>}/>
+      <Route path={"*"} element={<NotFoundPage/>}/>
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,9 @@
+const SearchForm = () => {
+	return (
+		<div className="SearchForm">
+			<h4>Hello word!</h4>
+		</div>
+	);
+};
+
+export default SearchForm;
